c24d7a71e61ab4e1b0c7509cf0afd44bda8a8c3f
Carlos Añorve <carlosaarivera23@gmail.com>
2022-10-17 00:48:15 -0500

    refactor(project): add base commando to manage endpoints

--- apm2/cli.py
+++ apm2/cli.py
@@ -3,7 +3,8 @@ from importlib.metadata import version as v
 import typer
 from dotenv import load_dotenv
 
-from apm2.commands.lambdas import lambda_app
+from apm2.commands.endpoints.commands import swagger_app
+from apm2.commands.lambdas.commands import lambda_app
 from apm2.commands.project.commands import project_app
 
 load_dotenv()
@@ -11,6 +12,7 @@ load_dotenv()
 app = typer.Typer()
 app.add_typer(lambda_app, name='lambda')
 app.add_typer(project_app, name='project')
+app.add_typer(swagger_app, name='endpoint')
 
 
 @app.callback(invoke_without_command=True)
