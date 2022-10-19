f0351d4026755b72357fa14bb7bb7a9672ee884f
Carlos Añorve <carlosaarivera23@gmail.com>
2022-10-19 00:09:06 -0500

    refactor(project): add logic to generate an initial project.

--- apm2/commands/project/actions.py
+++ apm2/commands/project/actions.py
@@ -1,31 +1,43 @@
 from cookiecutter.main import cookiecutter
 
 from apm2.utils.abstracts import BaseCommand
+import logging
+
+LOGGER = logging.getLogger("project_actions")
 
 
 class InitialProject(BaseCommand):
     def __init__(self, payload):
         super().__init__(payload)
+        LOGGER.debug("initializing initial project")
         self._prompt = self._payload["prompt"]
+        self._language = self._payload["language"]
         self._function = self.__default_project if self._payload["type"] else self.__custom_project
 
     def execute(self):
         self._function()
 
     def __default_project(self):
-        self.__generate_templates("https://github.com/aoricaan/apm-template.git")
+        self.__generate_templates("https://github.com/aoricaan/apm-template", self._language)
 
     def __custom_project(self):
-        print('Remember the project will be compatible with cookiecutter.')
+        LOGGER.info('Remember the project will be compatible with cookiecutter.')
         template = self._prompt("path to template: ")
-        self.__generate_templates(template)
+        folder = self._prompt("specific folder?: ")
+        args = (template, folder)
+        self.__generate_templates(*args)
 
     @staticmethod
-    def __generate_templates(package):
+    def __generate_templates(package, directory=None):
         """
         Generates the templates for the project.
         """
         try:
-            cookiecutter(template=package)
+            LOGGER.debug("building kwargs params.")
+            kwargs = dict(template=package)
+            if directory:
+                kwargs.update(directory=directory)
+            LOGGER.debug("building project.")
+            cookiecutter(**kwargs)
         except Exception as e:
-            print(f"{e}")
+            LOGGER.error(f"{e}")
--- apm2/commands/project/commands.py
+++ apm2/commands/project/commands.py
@@ -1,6 +1,7 @@
 import typer
 
 from apm2.commands.project.actions import InitialProject
+from apm2.utils.data_types import TemplateLanguages
 from apm2.utils.providers import BasicProvider
 
 project_app = typer.Typer()
@@ -11,10 +12,14 @@ provider = BasicProvider()
 def initialize_new_project(default: bool = typer.Option(False,
                                                         help="Create a new project with default template.",
                                                         prompt="Do you want to create a new project with "
-                                                               "default template?")):
+                                                               "default template?"),
+                           language: TemplateLanguages = typer.Option(TemplateLanguages.YAML,
+                                                                      help='Language to generate all templates.',
+                                                                      prompt='Ingress the language fot this project')):
     """
     Initialize a new empty project in the current path.
 
     """
-    provider.set_command(InitialProject({"type": default, "prompt": typer.prompt}))
+    provider.set_command(InitialProject({"type": default, "prompt": typer.prompt, "language": language}))
+    provider.execute_command()
     print('Project init successfully!')
